c0d5c8300a8c39d1cd344b9941974a7f8f5b4f49
Nahuel Dolores <contact@nahu.me>
2022-01-08 14:50:27 -0300

    Update to 1.18.1 and Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 import us.minevict.mvutilgradleplugin.*
 
 plugins {
-    id("us.minevict.mvutil") version "0.2.5"
+    id("us.minevict.mvutil") version "0.4.5"
 }
 
 group = "us.minevict"
-version = "0.1.2"
-mvUtilVersion = "6.0.1"
+version = "0.2.0"
+mvUtilVersion = "6.3.5"
 
 dependencies {
-    compileOnly(waterfallApi("1.15"))
+    compileOnly("io.github.waterfallmc:waterfall-api:1.18-R0.1-SNAPSHOT")
     compileOnly(mvutil("bungee"))
 }
 
